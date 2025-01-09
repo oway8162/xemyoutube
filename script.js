7aa95fca84d6357f202aafe5c004d6f88bee4fd6
oway8162 <oway8162@gmail.com>
2025-01-10 00:09:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ var objectElement = document.querySelector(".object");
 // var embedElenment = objectElement.querySelector(".embed");
 // var body = document.querySelector("body");
 
+function youtube_parser(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(live\/)|(embed\/)|(shorts\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return match && match[9].length == 11 ? match[9] : false;
+}
+
 const ytVideo = (youtubeID) => {
   objectElement.innerHTML = `
-  
-  <img loop class="catrun" src="catrun.gif" alt="this slowpoke moves" />
+  <img loop class="catwalk" src="catwalk.gif" alt="Cat Walk" />
     <param
       class="param"
       name="movie"
@@ -22,13 +28,25 @@ const ytVideo = (youtubeID) => {
     />`;
 };
 
-btn.addEventListener("click", () => {
-  //   console.log(link.value);
+function setCookie(key, value) {
+  document.cookie = key + "=" + value;
+}
 
-  let result = link.value.replace("https://www.youtube.com/watch?v=", "");
+function OpenVideo(linkVideo) {
+  let result = youtube_parser(linkVideo);
   ytVideo(result);
-  //   embedElenment.value = result;
-  //   paramElenment.value = result;
+}
+
+btn.addEventListener("click", () => {
+  setCookie("youtubeID", link.value);
+  OpenVideo(link.value);
+});
+
+link.addEventListener("keypress", function (event) {
+  if (event.which === 13) {
+    setCookie("youtubeID", link.value);
+    OpenVideo(link.value);
+  }
 });
 
 const btnChat = document.querySelector(".btnChat");
@@ -37,7 +55,30 @@ const popChat = document.getElementById("popChat");
 
 btnChat.addEventListener("click", () => {
   popChat.classList.toggle("hidechat");
-  console.log("dsffds");
 });
 
 // console.log(paramElenment.value);
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function LoadObject() {
+  let linkVideoCookie = getCookie("youtubeID");
+  OpenVideo(linkVideoCookie);
+  console.log(linkVideoCookie);
+}
+
+window.addEventListener("load", LoadObject);
