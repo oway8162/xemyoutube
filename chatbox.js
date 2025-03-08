8f51b65b60cc3c772f8de978b447c1d5fc116efa
oway8162 <oway8162@gmail.com>
2025-03-08 10:28:56 +0700

    Update chatbox.js

--- chatbox.js
+++ chatbox.js
@@ -18,6 +18,8 @@ import {
   child,
   get,
   set,
+  query,
+  limitToLast,
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
 const db = getDatabase();
@@ -29,10 +31,28 @@ const textMessenger = chatFoot.querySelector("input");
 
 const divElement = document.createElement("div");
 const spanElement = document.createElement("span");
+spanElement.classList.add("chatContent");
+const spanElement2 = document.createElement("span");
+spanElement2.classList.add("chatGetTime");
 divElement.classList.add("mess");
-divElement.appendChild(spanElement);
 
+divElement.appendChild(spanElement2);
+divElement.appendChild(spanElement);
 var getLengthMessenger = 0;
+
+function getCurrentTime() {
+  const d = new Date();
+  const timeWithoutSeconds = d.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(d.getDate()).padStart(2, "0");
+  const getD = day + "/" + month + "/" + year;
+  return `${timeWithoutSeconds} ${getD}`;
+}
 
 textMessenger.addEventListener("keypress", function (event) {
   if (event.which === 13) {
@@ -51,43 +71,80 @@ btnSend.addEventListener("click", () => {
   }
 });
 
+// const scrollToBottom = (id) => {
+//   const element = document.getElementById(id);
+//   element.scrollTop = element.scrollHeight;
+// };
+var MinusLimit;
 function GetMessenger() {
   const dbRef = ref(db);
   chatBody.innerHTML = "";
+
   get(child(dbRef, "/messenger")).then((messenges) => {
+    getLengthMessenger = messenges.val().length;
+    const limit = getLengthMessenger - 30;
+    MinusLimit = getLengthMessenger - limit + 1;
     messenges.forEach((messenge) => {
-      spanElement.innerHTML = messenge.val().text;
-      chatBody.innerHTML += divElement.outerHTML;
-      getLengthMessenger = messenges.val().length;
+      if (messenge.key > limit) {
+        spanElement.innerHTML = messenge.val().text;
+        spanElement2.innerHTML = messenge.val().time;
+        chatBody.innerHTML += divElement.outerHTML;
+      }
     });
+
+    chatBody.scrollTop = 999999999;
   });
 }
 
+const getTimeNow = getCurrentTime();
 function AddMessBoxChat() {
   spanElement.innerHTML = textMessenger.value;
+  spanElement2.innerHTML = getTimeNow;
   chatBody.innerHTML += divElement.outerHTML;
   textMessenger.focus();
   textMessenger.value = "";
+  chatBody.scrollTop = 999999999;
 }
 
 function AddText(textMessenge) {
   set(ref(db, "messenger/" + getLengthMessenger), {
     text: textMessenge,
+    time: getTimeNow,
   });
-
   getLengthMessenger++;
 }
 
-var checktime = getLengthMessenger;
+var checkLastMess = "";
+async function getLastMessages(numMessages) {
+  const lastMessagesQuery = query(
+    ref(db, "/messenger"),
+    limitToLast(numMessages)
+  );
+  const snapshot = await get(lastMessagesQuery);
+
+  if (snapshot.exists()) {
+    const messages = snapshot.val();
+    return Object.values(messages)[0].text;
+  } else {
+    console.log("No messages found");
+  }
+}
+
+var lastText = "";
 setInterval(() => {
-  get(child(ref(db), "/messenger")).then((messenges) => {
-    checktime = messenges.val().length;
+  getLastMessages(1).then((x) => {
+    if (x != "") {
+      lastText = x;
+    }
   });
-  if (checktime != chatBody.childElementCount) {
+  if (
+    lastText != chatBody.lastElementChild.lastElementChild.innerHTML &&
+    lastText != ""
+  ) {
     GetMessenger();
   } else {
     return;
   }
-}, 2000);
+}, 1000);
 
 window.addEventListener("load", GetMessenger);
